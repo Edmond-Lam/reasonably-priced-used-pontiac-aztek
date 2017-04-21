698c2d997b18bfdf5bcce28d5ea660bec3170ee0
Edmond-Lam <elam3@stuy.edu>
2017-04-21 09:48:21 -0400

    task two done

--- password.py
+++ password.py
@@ -1,5 +1,25 @@
+import math
+
 def taskOne(pswrd):
     a = [x for x in pswrd]
     return (not(not[x for x in a if x.isupper()])) and (not(not[x for x in a if x.islower()])) and (not(not[x for x in a if x.isdigit()]))
-    
+
 print taskOne("App1e")
+
+def taskTwo(pswrd):
+    print pswrd
+    a = [x for x in pswrd]
+    if taskOne(pswrd):
+        score = len([x for x in a if x.isupper()])/1.5 + len([x for x in a if x.isdigit()]) + len([x for x in a if x.islower()])/1.5 + len([x for x in a if x in ".?!&#,;:-_*"])
+        if score>10:
+            return 10;
+        else:
+            return int(math.floor(score) + 1);
+    else:
+        return "The password is not valid."
+
+print taskTwo("Grovyle_12251999")
+
+print taskTwo("groV?!1grov")
+
+        
