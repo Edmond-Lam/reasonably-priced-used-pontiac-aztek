c56e0f01006eea3c376d4f16948e55cfb07d4a54
fhaque1 <farhan3231@gmail.com>
2017-04-25 14:26:14 -0400

    Update password.py

--- password.py
+++ password.py
@@ -1,8 +1,7 @@
 import math
 
 def taskOne(pswrd):
-    a = [x for x in pswrd]
-    return (not(not[x for x in a if x.isupper()])) and (not(not[x for x in a if x.islower()])) and (not(not[x for x in a if x.isdigit()]))
+    return (not(not[x for x in pswrd if x.isupper()])) and (not(not[x for x in pswrd if x.islower()])) and (not(not[x for x in pswrd if x.isdigit()]))
 
 print taskOne("App1e")
 
